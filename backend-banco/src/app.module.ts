660c6e12680a4ec3d760fbdebc68e0daed7e17f3
Juan Camilo <ing.jcllamas@gmail.com>
2025-01-24 18:15:19 -0500

    feat: agrega soporte para archivos estáticos y actualiza dependencias de NestJS

--- backend-banco/src/app.module.ts
+++ backend-banco/src/app.module.ts
@@ -8,12 +8,18 @@ import { AuthModule } from './auth/auth.module';
 import { CreditoMiddleware } from './middleware/creditos.middleware';
 import { ClientesController } from './clientes/clientes.controller';
 import { ClientesModule } from './clientes/clientes.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: process.env.PATH_STATIC || join(__dirname, '..', 'public'),
+    }),
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.URI_MONGO_BANKING_SERVICE || 'mongodb://localhost:27017/banking'),
+    MongooseModule.forRoot(process.env.URI_MONGO_BANKING_SERVICE || 'mongodb+srv://jcllamas:CXATqCOhTAAyuf08@cluster0.pbxj3.mongodb.net/banking?retryWrites=true&w=majority&appName=Cluster0'),
+    // MongooseModule.forRoot(process.env.URI_MONGO_BANKING_SERVICE || 'mongodb://localhost:27017/banking'),
     CreditosModule, 
     AuthModule, 
     ClientesModule
